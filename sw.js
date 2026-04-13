0c99004aa19c37900c6539d94ff0156502237c4f
iamsantanuspeaking <iamsantanuspeaking@users.noreply.github.com>
2026-04-13 10:32:17 +0000

    deploy: 4b4ed392fdc1421c82c97ee33d8e889d575a7b2a

---
+++ sw.js
@@ -0,0 +1,43 @@
+const CACHE_NAME = 'sm-portfolio-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './questions.json',
+  './icon.png',
+  './santanu_portrait.png',
+  './article_ai.png',
+  './article_amazon.png',
+  './article_tpm.png'
+];
+
+// Install Event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching shell assets');
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Activate Event
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cacheRes) => {
+      return cacheRes || fetch(event.request);
+    })
+  );
+});
